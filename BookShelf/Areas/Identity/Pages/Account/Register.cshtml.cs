7ee5203c6fd586e8984835868ab89b00ff02e05a
VPN Shelley <shell@CoreyA.(none)>
2019-08-24 14:33:58 -0500

    register new user

--- BookShelf/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookShelf/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -55,6 +55,14 @@ namespace BookShelf.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
         }
 
         public void OnGet(string returnUrl = null)
@@ -67,7 +75,13 @@ namespace BookShelf.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser
+                {
+                    UserName = Input.Email,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    Email = Input.Email
+                };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
